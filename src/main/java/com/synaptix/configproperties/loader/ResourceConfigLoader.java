a5534b854035dd4b2b16cea0dd80655cb8d8483a
Gabriel Allaigre <gabriel.allaigre@talanlabs-com>
2016-05-12 10:40:29 +0200

    loader

---
+++ src/main/java/com/synaptix/configproperties/loader/ResourceConfigLoader.java
@@ -0,0 +1,16 @@
+package com.synaptix.configproperties.loader;
+
+import com.google.common.io.Resources;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ResourceConfigLoader extends URLConfigLoader {
+
+    private static final Logger LOG = LogManager.getLogger(ResourceConfigLoader.class);
+
+    public static final String DEFAULT_INTERNAL_PROPERTIES_PATH = "config.properties";
+
+    public ResourceConfigLoader(String internalPropertiesPath) {
+        super(Resources.getResource(internalPropertiesPath));
+    }
+}
